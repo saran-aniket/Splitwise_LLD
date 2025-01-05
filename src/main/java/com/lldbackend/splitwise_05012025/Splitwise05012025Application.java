a7c32f4d4a5de806984623b4744d0233c105d9e5
Aniket Sharan <sarananiketgupta1610@gmail.com>
2025-01-05 21:13:21 +0530

    Initial Commit

---
+++ src/main/java/com/lldbackend/splitwise_05012025/Splitwise05012025Application.java
@@ -0,0 +1,13 @@
+package com.lldbackend.splitwise_05012025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Splitwise05012025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Splitwise05012025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lldbackend/splitwise_05012025/Splitwise05012025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lldbackend.splitwise_05012025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Splitwise05012025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
